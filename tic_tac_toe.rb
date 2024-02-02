d0f9017af068c802e7809c581013b0060ff17c2e
getDub <143089345+getDub@users.noreply.github.com>
2024-02-02 22:25:37 +1100

    Gets now chomps to allow 'if' statement to work.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,17 +4,23 @@ LINE = [[1,2,3],[4,5,6],[7,8,9]]
 
 column = " | "
 $row = "--+---+--"
-crosses = "X"
-naughts = "0"
 
 
 def players
-puts "Player1, naughts or crosses?"
-player1 = Kernel.gets.match(/(naughts|crosses)/) #find symbol for caps or lower case and default for non- match
-puts "player1 chose #{player1}"
-puts "Player 2, you will be #{}.\n LETS GO!"
+  crosses = "X"
+  naughts = "0"
+  puts "Player1, naughts or crosses?"
+  player1 = Kernel.gets.chomp #find symbol for caps or lower case and default for non- match
+if player1 == "crosses"
+  player1 = crosses
+  player2 = naughts
+else
+    player1 = naughts
+    player2 = crosses
 end
-
+puts "Player 1 chose #{player1}'s"
+puts "Player 2, you will be #{player2}'s.\n LETS GO!"
+end
 players
 
 # LINE[0][0] = crosses
