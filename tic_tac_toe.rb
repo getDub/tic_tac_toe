f9249b150ca8d82123c35c1752eda465fabe76bc
getDub <143089345+getDub@users.noreply.github.com>
2024-02-02 21:24:00 +1100

    Layout board and ask players, 'naughts or crosses?'.
    Brain dump at this stage. Nothing working as it should.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,1 +1,32 @@
 #Project: tic tac toe
+
+LINE = [[1,2,3],[4,5,6],[7,8,9]]
+
+column = " | "
+$row = "--+---+--"
+crosses = "X"
+naughts = "0"
+
+
+def players
+puts "Player1, naughts or crosses?"
+player1 = Kernel.gets.match(/(naughts|crosses)/) #find symbol for caps or lower case and default for non- match
+puts "player1 chose #{player1}"
+puts "Player 2, you will be #{}.\n LETS GO!"
+end
+
+players
+
+# LINE[0][0] = crosses
+# LINE[1][1] = naughts
+
+
+
+def board
+LINE.each do |line|
+  puts line.join(' | ')
+  puts $row 
+end
+end
+
+board
