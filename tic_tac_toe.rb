b48a6d1e5012048dc086a4b327d5b6dc9283c127
getDub <143089345+getDub@users.noreply.github.com>
2024-02-05 19:48:36 +1100

    Board method houses column and row variables. They're not global anymore

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,25 +3,23 @@
 
 LINE = [[1,2,3],[4,5,6],[7,8,9]]
 
-column = " | "
-$row = "--+---+--"
 
 
 def players
-  crosses = "X"
-  naughts = "0"
-  puts "Player1, naughts or crosses?"
-  player1 = Kernel.gets.chomp #find symbol for caps or lower case and default for non- match
-if player1 == "crosses"
-  player1 = crosses
-  player2 = naughts
-else
-    player1 = naughts
-    player2 = crosses
-end
-# puts "Player 1 chose #{player1}'s"
-puts "Player 1 chose crosses"
-puts "Player 2, you will be #{player2}'s.\n LETS GO!"
+    crosses = "X"
+    naughts = "0"
+    puts "Player1, naughts or crosses?"
+    player1 = Kernel.gets.chomp #find symbol for caps or lower case and default for non- match
+    if player1 == "crosses"
+        player1 = crosses
+        player2 = naughts
+    else
+        player1 = naughts
+        player2 = crosses
+    end
+    # puts "Player 1 chose #{player1}'s"
+    puts "Player 1 chose crosses"
+    puts "Player 2, you will be #{player2}'s.\n LETS GO!"
 end
 players
 
@@ -35,22 +33,17 @@ LINE.each_with_index do |array_value, array_index|
             LINE[array_index][sub_array_index] = "X"
 
         end
+    end
 end
-end
-
-
-
-
-# LINE[0][0] = crosses
-# LINE[1][1] = naughts
-
 
 
 def board
-LINE.each do |line|
+    column = " | "
+    row = "--+---+--"
+    LINE.each do |line|
     puts line.join(' | ')
     if line[2] == 3 || line[2] == 6
-    puts $row
+    puts row
 end
 end
 end
