02d9d1ad3472985c05fb859742dba34a292259a0
getDub <143089345+getDub@users.noreply.github.com>
2024-02-05 20:35:34 +1100

    Players method if statement converted to a ternary expression

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -6,19 +6,14 @@ LINE = [[1,2,3],[4,5,6],[7,8,9]]
 
 
 def players
-    crosses = "X"
-    naughts = "0"
-    puts "Player1, naughts or crosses?"
-    player1 = Kernel.gets.chomp #find symbol for caps or lower case and default for non- match
-    if player1 == "crosses"
-        player1 = crosses
-        player2 = naughts
-    else
-        player1 = naughts
-        player2 = crosses
+    player1 = ""
+    player2 = ""
+    puts "Player1, would you like to be Naughts (press O) or Crosses (press X)"
+    player1_choice = Kernel.gets.match(/x|X|o|O|0/)
+    player1 = player1_choice.string.upcase.chomp
+    if player1 == "X"? player2 = "O" : player2 = "X"
     end
-    # puts "Player 1 chose #{player1}'s"
-    puts "Player 1 chose crosses"
+    puts "Player 1 chose #{player1}'s"
     puts "Player 2, you will be #{player2}'s.\n LETS GO!"
 end
 players
