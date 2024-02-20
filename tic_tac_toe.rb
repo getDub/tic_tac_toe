2b4a595342e4453f18d141a0cae892171514019a
getDub <143089345+getDub@users.noreply.github.com>
2024-02-20 12:25:44 +1100

    Current player is in initialize method now so it can be called in other methods of self.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,67 +1,74 @@
 
 #Project: tic tac toe
 
-
-
 class Player
-  attr_accessor :player, :marker, :marker_name, :player_indice
+  attr_accessor :player_name, :marker_symbol, :marker_name, :player_indice
 
   @@number_of_players = 0
 
   def initialize
-    @player = player_name
-    @marker = marker
+    @player_name = player_name
+    @marker_symbol = marker_symbol
     @marker_name = marker_name
     @player_indice = player_indice #should this be in the game engine
     @@number_of_players += 1
   end
-
+  
   def self.total_no_of_players
     @@number_of_players
   end
-    
+
   def player_name
-    puts "Hi player! What is your name?"
-    @player = gets.chomp
+    puts "Hi player! What is your name?" 
+    reply = gets.chomp
+    @player_name = reply
   end
 
-  def marker
-    puts "#{player}, would you like to be Naughts (press O) or Crosses (press X)"
-    players_choice = Kernel.gets.match(/x|X|o|O|0/)
-    return @marker = players_choice.string.upcase.chomp
+  def p_name
+    @player_name
+  end
+
+  def marker_symbol
+    puts "#{@player_name}, would you like to be Naughts (press O) or Crosses (press X)"
+    players_choice = Kernel.gets.chomp.match(/x|X|o|O|0/)
+    return @marker_symbol = players_choice.string.upcase
   end
   
   def info 
-    puts "Player name = #{self.player}, and they chose #{self.marker_name}(#{self.marker})"
+    puts "Player name = #{@player_name}, and they chose #{@marker_name}(#{@marker_symbol})"
   end
 
-  private
+  def mark_name
+    @marker_name
+  end
 
   def marker_name
-    if @marker == "X" ? @marker_name = "crosses" : @marker_name = "naughts"
+    if @marker_symbol == "X" ? @marker_name = "crosses" : @marker_name = "naughts"
     end
-    puts "Thanks #{@player}, you're #{@marker_name}(#{@marker})'s for this game."
+    puts "Thanks #{@player_name}, you're #{@marker_name}(#{@marker_symbol})'s for this game."
   end
 end
 
-# p1 = Player.new
+#-----------------------------------------------
 
-
-
-    
-class Game_Engine
+class Game_Engine < Player
 
   LINE = [[1,2,3],[4,5,6],[7,8,9]]
-
-  @current_name_indice = 1
-  # @player = current_player
   
-  attr_accessor :players, :board
-
+  @current_name_indice = 1
+  
+  attr_accessor :players, :board, :current_player
+  
   def initialize
     @players = [Player.new, Player.new]
     @board = board
+    @current_player = current_player
   end
+
+  # def player_name
+  #   puts "Hi player! What is your name?" 
+  #   @player_name = gets.chomp
+  # end
 
   def board
     column = " | "
@@ -74,15 +81,15 @@ class Game_Engine
   end
 
   def current_player
-    return @players[@current_name_indice]
+    return @players[@current_name_indice.to_i].p_name
   end
 
   def next_player
-    return @current_name_indice = (@current_name_indice + 1)% @players.size
+    return @current_name_indice = (@current_name_indice.to_i + 1)% @players.size
   end
 
   def player_move
-    puts "#{current_player}, select a number on the grid to place your #{@current_player}."
+    puts "#{@current_player}, select a number on the grid to place your #{@mark_name}."
     player_grid_selection = Kernel.gets.match(/[1-9]/)
     selection = player_grid_selection.to_s
     grid_integer = selection.to_i
@@ -101,3 +108,8 @@ class Game_Engine
 end
 
 new_game = Game_Engine.new
+# p new_game.current_player
+# new_game.next_player
+p new_game.current_player
+p new_game.player_move
+# p new_game.player_name
